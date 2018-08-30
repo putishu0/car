8348dc9068a5b43acf478afbb4607f036c4fce77
putishu0 <39542520+putishu0@users.noreply.github.com>
2018-08-30 09:05:02 +0800

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -140,70 +140,81 @@ var banner = function(){
         distanceX = 0;
         ismove = false;
     });
-}
-
 
 
 
 // 第二个轮播图
-	var oBox=document.getElementById('box');
-	var oUl1=document.getElementById('ul1');
-	var oLi1=oUl1.getElementsByTagName('li');
-	var oUl2=document.getElementById('ul2');
-	var oLi2=oUl2.getElementsByTagName('li');
-	var oLi1Width=oLi1[0].offsetWidth;
-	var timer=null;
-	var time=null;
-	var y=0;
-	//alert(oLi1Width);
-	oUl1.style.width=oLi1Width*oLi1.length+'px';    //给ul1一个宽度
+// 	var oBox=document.getElementById('box');
+// 	var oUl1=document.getElementById('ul1');
+// 	var oLi1=oUl1.getElementsByTagName('li');
+// 	var oUl2=document.getElementById('ul2');
+// 	var oLi2=oUl2.getElementsByTagName('li');
+// 	var oLi1Width=oLi1[0].offsetWidth;
+// 	var timer=null;
+// 	var time=null;
+// 	var y=0;
+// 	//alert(oLi1Width);
+// 	oUl1.style.width=oLi1Width*oLi1.length+'px';    //给ul1一个宽度
 
-	for(var i=0;i<oLi2.length;i++){
-		//alert(i)
-		oLi2[i].index=i;   //获取li2的索引
-		oLi2[i].onmouseover=function(){
-			show(this.index)
-			// alert(this.index)
-		}
-	}
+// 	for(var i=0;i<oLi2.length;i++){
+// 		//alert(i)
+// 		oLi2[i].index=i;   //获取li2的索引
+// 		oLi2[i].onmouseover=function(){
+// 			show(this.index)
+// 			// alert(this.index)
+// 		}
+// 	}
 
-	oBox.onmouseover=function(){      //鼠标放上定时器停止
-		clearInterval(time);
-	}
+// 	oBox.onmouseover=function(){      //鼠标放上定时器停止
+// 		clearInterval(time);
+// 	}
 
-	oBox.onmouseout=function(){       //鼠标离开开启定时器
-		time=setInterval(function(){
-			show(y);
-			y++;
-			//alert(oLi2.length)
-			if(y==oLi2.length){y=0}    
-		},2000)
-	}
+// 	oBox.onmouseout=function(){       //鼠标离开开启定时器
+// 		time=setInterval(function(){
+// 			show(y);
+// 			y++;
+// 			//alert(oLi2.length)
+// 			if(y==oLi2.length){y=0}    
+// 		},2000)
+// 	}
 
-	clearInterval(time);
-	time=setInterval(function(){      //自动轮播
-		show(y);
-		y++;
-		//alert(oLi2.length)
-		if(y==oLi2.length){y=0}    //图片轮播结束，拽回第一个重新开始；
-	},2000)
+// 	clearInterval(time);
+// 	time=setInterval(function(){      //自动轮播
+// 		show(y);
+// 		y++;
+// 		//alert(oLi2.length)
+// 		if(y==oLi2.length){y=0}    //图片轮播结束，拽回第一个重新开始；
+// 	},2000)
 
-	function show(j){         //鼠标放上li2对应的li运动并且li2对应的颜色改变
-		for(var i=0;i<oLi2.length;i++){
-				oLi2[i].className="";
-			}
-			oLi2[j].className="active";
+// 	function show(j){         //鼠标放上li2对应的li运动并且li2对应的颜色改变
+// 		for(var i=0;i<oLi2.length;i++){
+// 				oLi2[i].className="";
+// 			}
+// 			oLi2[j].className="active";
 
-			clearInterval(timer);
-			timer=setInterval(function(){
-				var iSpeed=(-oUl1.offsetLeft-oLi1Width*j)/6;     //缓冲运动
-				var x=iSpeed>0?Math.ceil(iSpeed):Math.floor(iSpeed);
-				oUl1.style.left=x+oUl1.offsetLeft+'px';
-			},22)
-	}
+// 			clearInterval(timer);
+// 			timer=setInterval(function(){
+// 				var iSpeed=(-oUl1.offsetLeft-oLi1Width*j)/6;     //缓冲运动
+// 				var x=iSpeed>0?Math.ceil(iSpeed):Math.floor(iSpeed);
+// 				oUl1.style.left=x+oUl1.offsetLeft+'px';
+// 			},22)
+// 	}
 
 
-// 表单清空
-$('.input1').click(function(){
-        this.value=""
- })
+// // 表单清空
+// $('.input1').click(function(){
+//         this.value=""
+//  })
+
+
+     var mySwiper = new Swiper(".swiper-container",{ 
+ direction:"horizontal",/*横向滑动*/ 
+ loop:true,/*形成环路（即：可以从最后一张图跳转到第一张图*/ 
+ pagination:".swiper-pagination",/*分页器*/ 
+ prevButton:".swiper-button-prev",/*前进按钮*/ 
+ nextButton:".swiper-button-next",/*后退按钮*/ 
+ autoplay:3000/*每隔3秒自动播放*/ 
+ }) 
+
+}
+
