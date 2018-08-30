39dc6dac6cef8c02f85b3bc013780681f608d010
putishu0 <39542520+putishu0@users.noreply.github.com>
2018-08-30 09:12:24 +0800

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -207,14 +207,5 @@ var banner = function(){
 //  })
 
 
-     var mySwiper = new Swiper(".swiper-container",{ 
- direction:"horizontal",/*横向滑动*/ 
- loop:true,/*形成环路（即：可以从最后一张图跳转到第一张图*/ 
- pagination:".swiper-pagination",/*分页器*/ 
- prevButton:".swiper-button-prev",/*前进按钮*/ 
- nextButton:".swiper-button-next",/*后退按钮*/ 
- autoplay:3000/*每隔3秒自动播放*/ 
- }) 
-
 }
 
